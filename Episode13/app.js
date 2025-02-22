1d05bf3f69db085ae4f747a75cba7ba4505d5e1a
Ankitha-kulal <ankithakulal303@gmail.com>
2025-02-22 12:22:39 +0000

    Added next last folder for the course

--- Episode13/app.js
+++ Episode13/app.js
@@ -1,13 +1,16 @@
-const BASE_URL =
-    "https://api.frankfurter.app/latest";
+const BASE_URL = "https://api.frankfurter.app/latest";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
 
 for (let select of dropdowns) {
-    for (currCode in countryList) {
+    for (let currCode in countryList) {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
-        newOption.value = currCode;
         newOption.value = currCode;
         if (select.name === "from" && currCode === "USD") {
             newOption.selected = "selected";
@@ -16,9 +19,52 @@ for (let select of dropdowns) {
         }
         select.append(newOption);
     }
-
     select.addEventListener("change", (evt) => {
         updateFlag(evt.target);
     });
-
 }
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    if (fromCurr.value === toCurr.value) {
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${amtVal} ${toCurr.value}`;
+        return;
+    }
+
+    const URL = `${BASE_URL}?from=${fromCurr.value}&to=${toCurr.value}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    let rate = data.rates[toCurr.value];
+
+    if (!rate) {
+        msg.innerText = "Exchange rate not available.";
+        return;
+    }
+
+    let finalAmount = (amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    if (img) img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", (evt) => {
+    updateExchangeRate();
+});
